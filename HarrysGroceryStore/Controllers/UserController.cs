90c8ee1e9d93142cb8a8c7623fdb8a9cf5d1e832
Harry Ezeodum <harryezeodum@yahoo.com>
2021-04-06 18:07:12 -0500

    Initial Commit

---
+++ HarrysGroceryStore/Controllers/UserController.cs
@@ -0,0 +1,128 @@
+﻿using HarrysGroceryStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrysGroceryStore.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(User userSignUp)
+        {
+            if (ModelState.IsValid)
+            {
+                if (userSignUp.PassWord == userSignUp.ConfirmPassword)
+                {
+                    Repository.AddResponse(userSignUp);
+                    return RedirectToAction("SignOnPage");
+                }
+                ModelState.AddModelError("", "Password don't match");
+            }
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogIn(User userLogIn)
+        {
+            if (ModelState.IsValid)
+            {
+                if (userLogIn.PassWord == userLogIn.PassWord)
+                {
+                    Repository.AddResponse(userLogIn);
+                    return RedirectToAction("SignOnPage");
+                }
+                ModelState.AddModelError("", "Password don't match");
+            }
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Admin(User adminLogIn)
+        {
+            if (ModelState.IsValid)
+            {
+                if (adminLogIn.PassWord == adminLogIn.PassWord)
+                {
+                    Repository.AddResponse(adminLogIn);
+                    return RedirectToAction("");
+                }
+                ModelState.AddModelError("", "Password don't match");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ResetPassword(User reset)
+        {
+            return View();
+        }
+
+        public IActionResult SignedOn()
+        {
+            return View();
+        }
+
+        public ViewResult ProfileDetails()
+        {
+            User u = new User();
+            u.FirstName = "John";
+            u.LastName = "Doe";
+            u.Email = "james.michael@aol.com";
+            u.PhoneNumber = "713-290-2346";
+            u.PassWord = "abcd";
+            return View(u);
+        }
+
+        [HttpGet]
+        public IActionResult EditProfile(string firstName)
+        {
+            User information = new User();
+            information.FirstName = firstName;
+            information.LastName = "Smith";
+            information.Email = "michael.smith@aol.com";
+            information.PhoneNumber = "210-712-2100";
+            information.PassWord = "1234";
+            information.ConfirmPassword = "1234";
+            return View(information);
+        }
+
+        [HttpPost]
+        public IActionResult EditProfile(User edit)
+        {
+            if (ModelState.IsValid)
+            {
+                if (edit.PassWord == edit.ConfirmPassword)
+                {
+                    Repository.EditResponse(edit);
+                    return RedirectToAction("ProfileDetails", edit); ;
+                }
+                ModelState.AddModelError("", "Password don't match");
+            }
+            return View(edit);
+        }
+    }
+}
---
+++ HarrysGroceryStore/Models/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrysGroceryStore.Models
+{
+    public class Repository
+    {
+        public static List<User> responses = new List<User>();
+        public static IEnumerable<User> Responses = responses;
+        public static void AddResponse(User response)
+        {
+            responses.Add(response);
+        }
+
+        public static void EditResponse(User response)
+        {
+            responses.Add(response);
+
+        }
+    }
+}
